d30b26c588f4cb111cc569a672bd13e07893345b
Jaouad2982 <Jadsarouti@outlook.com>
2017-11-21 02:45:24 -0500

    initial commit

---
+++ src/carproject/Main.java
@@ -0,0 +1,14 @@
+package carproject;
+//Jaouad Sarouti  a lover of programming
+public class Main {
+
+    public static void main(String[] args) {
+
+    	Fiat outlander = new Fiat(36);
+        outlander.steer(45);
+        outlander.accelerate(30);
+        outlander.accelerate(20);
+        outlander.accelerate(-42);
+
+    }
+}
